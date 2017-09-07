96015a4aebdd3c99f4d5269ba5ed046b620c7ae0
raku <yourmom@urmom.com>
2017-09-07 14:20:39 -0700

    Pre-refactoring commit

--- lists/tests.py
+++ lists/tests.py
@@ -6,10 +6,22 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         #Manually call glient.get and pass url we want to test
         response = self.client.get('/')
 
         #Django test class.
-        self.assertTemplateUsed(response, 'home.html')                              
+        self.assertTemplateUsed(response, 'home.html')
+        
+    def test_can_save_a_POST_request(self):
+        #Do a post- call self.client.post, includes data which contains data.
+
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        #Check that the text from POST request ends up in rendered HTML.
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+
+        
 
--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -40,21 +40,28 @@ class NewVisitorTest(unittest.TestCase):
         #returns element
         table = self.browser.find_element_by_id('id_list_table')
         #returns list of elements
+        #f-statements: Prepend a string with an f, and then use the curly bracked syntax
+        #to insert local variables.
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
         
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         #There is still a text box to add another item.  She adds "Make fly"
-        self.fail('Finish the Test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         #The page updates again, and shows both items.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
+        
         #Edith wonders whether the site will remember he list.
         #She sees that the site had generated a unique URL for her
         #There is some explanatory text to that effect
-
+        self.fail("Finish the test!")
         #She visits that URL, her to do list is still there
 
         #Satisfied, she goes back to sleep
