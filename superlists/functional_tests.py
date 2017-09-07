2fb05c88f737b9ae4fe45c46d10281e5973118c9
raku <yourmom@urmom.com>
2017-09-07 14:29:23 -0700

    Commit changes to functional_tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     #Any method whos name starts with test is a test method
     #and will be run by the test runner.
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -37,26 +42,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        #returns element
-        table = self.browser.find_element_by_id('id_list_table')
-        #returns list of elements
-        #f-statements: Prepend a string with an f, and then use the curly bracked syntax
-        #to insert local variables.
-        rows = table.find_elements_by_tag_name('tr')
-        
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         #There is still a text box to add another item.  She adds "Make fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        #The page updates again, and shows both items.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        
+        
 
+        #The page updates again, and shows both items.
         
         #Edith wonders whether the site will remember he list.
         #She sees that the site had generated a unique URL for her
