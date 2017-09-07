9deccfb66013e7d71ee8ae865f6353892ad18663
raku <yourmom@urmom.com>
2017-09-07 09:54:47 -0700

    aby's first URL

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
 
-    def test_bad_maths(self):
-        self.assertEqual( 1 + 1, 3)
+        #internally resolves urls and find which view function they map to.
+        #Were check that resolve, when called with '/', finds a function
+        #called home_page
         
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+        
+
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+#from django.Http import HttpResponse
+
+
+
+def home_page():
+    pass
+#    return HttpResponse("Welcome to our homepage!")
+
 
 # Create your views here.
