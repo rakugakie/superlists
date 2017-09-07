7f8cdcfa42ac82b4f93aca474e209cf301deb8b5
raku <yourmom@urmom.com>
2017-09-07 11:38:50 -0700

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -5,36 +5,11 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    
-    def test_root_url_resolves_to_home_page_view(self):
-
-        #internally resolves urls and find which view function they map to.
-        #Were check that resolve, when called with '/', finds a function
-        #called home_page
-        
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        #Create a httpRequest Object, which django
-        #will see when a users browser asks for a page
-        
-        request = HttpRequest()
+        #Manually call glient.get and pass url we want to test
+        response = self.client.get('/')
 
-        #We pass the request to our homepage, qhich gives it a response.
-        #This response is an instance of class HttpResponse
-        response = home_page(request)
-
-        #Then, we extract the .content of the request.  We decode it with
-        #.decode() to convert them into a string of HTML thats being sent to the user
-        html = response.content.decode('utf8')
-
-        
-        self.assertTrue(html.startswith('<html>'))
-
-        #We Want a Title of To-Do list, because that's what we specified in
-        #our functional test
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-                                      
+        #Django test class.
+        self.assertTemplateUsed(response, 'home.html')                              
 
--- lists/views.py
+++ lists/views.py
@@ -5,8 +5,5 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    pass
-    return HttpResponse('<html><title>To-Do lists</title></html>')
 
-
-# Create your views here.
+    return render(request, 'home.html')
