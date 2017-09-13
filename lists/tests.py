0379a37895d8fa71870c4aff3d51770154234ef5
Sal <go@fckurself.com>
2017-09-12 17:52:57 -0700

    I forget did I commit already

--- lists/tests.py
+++ lists/tests.py
@@ -34,7 +34,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):     
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         
     def test_only_saves_items_when_necessary(self):
         
@@ -58,5 +58,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, first_item.text)
         self.assertEqual(second_saved_item.text, second_item.text)
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemy 1')
+        Item.objects.create(text='itemy 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemy 1')
+        self.assertContains(response, 'itemy 2')
+
+
         
 
