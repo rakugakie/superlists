f8fcdb664d0a53f7795e00fbba2a348ce6ea6981
Sal <go@fckurself.com>
2017-09-19 17:37:29 -0700

    Added lists to model. End of 7.10

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+
+class List(models.Model):
+    url = models.URLField(max_length=100, default='')
+
+    def __str__(self):
+        return self.url
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 from lists.views import home_page
 
@@ -15,30 +15,49 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+
+        # Create list object
+        list_ = List()
+        list_.save()
+
+        #Create two item objects
         first_item = Item()
         first_item.text = 'The first ever list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
 
+        # assert that the list_ is the first list in DB
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        # assert there are two saved items
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
+
+        # assert some shit, including lists
         self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, second_item.text)
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemy 1')
-        Item.objects.create(text='itemy 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemy 1', list=list_)
+        Item.objects.create(text='itemy 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -65,5 +84,3 @@ class NewListTest(TestCase):
         # Assertion takes response, expected_url, status code (Default 302),
         # Target status code (default 200), mesage prefix,
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
-
-
